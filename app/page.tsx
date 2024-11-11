df816ee2c8987e0288bde24ff5d01920b6c141b7
Ali Rangwala <alirangwala@Alis-MacBook-Pro.local>
2024-11-10 17:51:53 -0800

    cleaned up prisma and auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
 import Game from "@/components/Game/Game";
 import Title from "@/components/Title/Title";
-import {prisma} from "@/lib/prisma"
+// import {prisma} from "@/lib/prisma"
 
 export default async function Home() {
 
-  const users = await prisma.user.findFirst()
-  console.log(users)
-  const user = await prisma.user.findFirst({
-    where: {
-      email: "test@test.com"
-    }
-  })
+  // const users = await prisma.user.findFirst()
+  // console.log(users)
+  // const user = await prisma.user.findFirst({
+  //   where: {
+  //     email: "test@test.com"
+  //   }
+  // })
   return (
    <>
-   Hello, {user?.email}
    <Title title = {"Periodic Tordle"} subtitle = {"Can you guess the Element?"} />
    <Game />
    </>
